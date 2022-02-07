a4ad05941eaa709db94959a6f1dcb09d6fa90132
Alex Bott <78749468+AlexPicchu@users.noreply.github.com>
2022-02-07 11:34:11 +0000

    Add files via upload

--- tail_v_calculation.m
+++ tail_v_calculation.m
@@ -4,13 +4,13 @@ clear, clc
 % all results are relative to mu
 
 f = 35; %frequency (1/s)
-L = [24;48;96;192;500;1000]; %wavelength (mu)
-b = 1:1000; %amplitude (mu)
-n = 100; %number of waves 
+L = linspace(0.001,100,100); %wavelength (mu)
+b = linspace(0.4,4,10)'; %amplitude (mu)
+n = 1.3; %number of waves 
 a = 0.5; %radius of head (mu)
 d = 0.2; %radius of tail (mu)
 
 speed = Tail_velocity(f, L, b, n, a, d);
-
-plot(b,speed)
-grid on
+%plot(b,speed)
+surf(L,b,speed)
+grid off
